56ab5dacdb57525fb226d0530553a4e2fec2b2a7
WalterJhonSalibay <salibaywaltz@gmail.com>
2024-10-04 17:26:22 +0800

    #44displayimage

---
+++ src/app/components/displayimage/displayimage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-displayimage',
+  standalone: true,
+  imports: [],
+  templateUrl: './displayimage.component.html',
+  styleUrl: './displayimage.component.css'
+})
+export class DisplayimageComponent {
+
+}
